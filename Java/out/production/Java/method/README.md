2bdfb8f0d7efaad7297247299c90d11ee903f2ab
KimHunJin <ysg01129@naver.com>
2018-07-14 23:55:26 +0900

    [date] 2018.07.14 (Set.)
    [add]
    1. lambda

---
+++ Java/out/production/Java/method/README.md
@@ -0,0 +1,36 @@
+## Method
+
+함수.
+
+리턴 타입에 따라서 void, String, char, bool, int, long 등으로 만들 수 있음
+
+```
+
+void testMethod() {
+    // TODO ; your method or function
+}
+
+```
+
+### 오버로딩
+
+1. 같은 메서드
+2. 파라미터 수가 다름 or
+3. 파라미터 타입이 다름
+
+(단, 리턴 값만 다를 때는 사용 불가 - 에러)
+
+```
+void testMethod() {
+
+}
+
+void testMethod(int a) {
+
+}
+
+void testMethod(String s) {
+
+}
+
+```
---
+++ Java/src/lambda/Lambda.java
@@ -0,0 +1,115 @@
+package lambda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Lambda {
+
+    class Apple {
+        int weight;
+
+        Apple(int weight) {
+            this.weight = weight;
+        }
+
+        Apple getWeight() {
+            return this;
+        }
+
+        int compareTo(Apple o) {
+            return Integer.compare(weight, o.weight);
+        }
+    }
+
+    private void originalMethod() {
+        Comparator<Apple> byWeight = new Comparator<Apple>() {
+            @Override
+            public int compare(Apple o1, Apple o2) {
+                return o1.getWeight().compareTo(o2.getWeight());
+            }
+        };
+    }
+
+    private void lambdaMethod() {
+        // lambda
+        Comparator<Apple> byWeight = (Apple o1, Apple o2) ->
+                o1.getWeight().compareTo(o2.getWeight());
+
+        // method reference
+        Comparator<Apple> weight = (Apple::compareTo);
+    }
+
+    /**
+     * using lambda
+     */
+    Runnable r1 = () -> System.out.println("hello world 1");
+
+    /**
+     * using anonymous class
+     */
+    Runnable r2 = new Runnable() {
+        @Override
+        public void run() {
+            System.out.println("hello world 2");
+        }
+    };
+
+    private static void process(Runnable r) {
+        r.run();
+    }
+
+    private void test() {
+        process(r1);
+        process(r2);
+        process(() -> System.out.println("hello world 3")); // using lambda
+    }
+
+    /**
+     * auto boxing
+     * int -> Integer
+     * <p>
+     * 변환 과정에 비용이 소모됨.
+     * 기본형을 감싸는 레퍼가 되어 힙에 저장됨.
+     */
+    private void autoBoxing() {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 300; i < 400; i++) {
+            list.add(i);
+        }
+    }
+
+    private interface IntPredicate {
+        boolean test(int t);
+    }
+
+    /**
+     * not boxing
+     */
+    private void notBoxing() {
+        IntPredicate evenNumbers = (int i) -> i % 2 == 0;
+        evenNumbers.test(1000);
+    }
+
+    /**
+     * boxing
+     */
+    private void boxing() {
+        Predicate<Integer> oddNumbers = (Integer i) -> i % 2 == 1;
+        oddNumbers.test(1000);
+    }
+
+    private void methodReference() {
+
+        String[] s = new String[]{"3", "4", "2", "1", "5"};
+        List<String> list = Arrays.asList(s);
+
+        list.forEach(x -> System.out.println(x)); // lambda
+        list.forEach(System.out::println); // method reference
+    }
+
+    public static void main(String[] args) {
+    }
+}
