5323bb91179379ec93729978913a5d636e6a9622
KimHunJin <ysg01129@naver.com>
2018-08-05 15:30:09 +0900

    [date] 2018.08.05 (Sun.)
    [add]
    1. baekjoon 2775

---
+++ algorithm/src/baekjoon/BJ_2775.java
@@ -0,0 +1,58 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_2775 {
+    public static void main(String[] args) {
+        new BJ_2775().solve();
+    }
+
+    private void solve() {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = convertInt(input(br));
+            for(int i=0;i<n;i++) {
+                unitTest(br);
+            }
+        } catch (IOException ie) {
+            ie.printStackTrace();
+        }
+    }
+
+    private void unitTest(BufferedReader br) {
+        int n = convertInt(input(br));
+        int k = convertInt(input(br));
+
+        int[][] map = new int[n+1][k];
+        for(int i=0;i<k;i++) {
+            map[0][i] = i+1;
+        }
+
+        for(int i=1;i<=n;i++) {
+            for(int j=0;j<k;j++) {
+                int sum = 0;
+                for(int t=0;t<=j;t++) {
+                    sum += map[i-1][t];
+                }
+                map[i][j] = sum;
+            }
+        }
+
+        System.out.println(map[n][k-1]);
+    }
+
+    private int convertInt(String s) {
+        return Integer.parseInt(s);
+    }
+
+    private String input(BufferedReader br) {
+        String s = "";
+        try {
+            s = br.readLine();
+        } catch (IOException ie) {
+            ie.printStackTrace();
+        }
+        return s;
+    }
+}
