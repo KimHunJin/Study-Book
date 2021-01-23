fc30a515107e46e9a1e5ee361a142d064f0e3467
HunJin Kim <ysg01129@naver.com>
2021-01-23 16:39:21 +0900

    [알고리즘][문제풀이] 미니멈 오퍼레이션 투 리듀스 투 제로 풀이
    - 메모리 초과 ㅠㅠ

---
+++ algorithm/src/leetcode/MinimumOperationsToReduceXToZero.java
@@ -0,0 +1,69 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 메모리 초과 ㅠㅠ
+ */
+public class MinimumOperationsToReduceXToZero {
+
+    public static void main(String[] args) {
+        new MinimumOperationsToReduceXToZero();
+    }
+
+    public int minOperations(int[] nums, int x) {
+        int lastindex = nums.length - 1;
+        int index = 0;
+
+        List<Info> infoQueue = new ArrayList<>();
+        infoQueue.add(new Info(1, x - nums[0], 1, lastindex));
+        infoQueue.add(new Info(1, x - nums[lastindex], 0, lastindex - 1));
+        while (index != infoQueue.size()) {
+            Info info = infoQueue.get(index);
+            index++;
+
+            if (info.remainder == 0) {
+                return info.count;
+            }
+
+            if (info.remainder < 0) {
+                continue;
+            }
+
+            if (info.leftIndex >= nums.length || info.rightIndex <= 0) {
+                continue;
+            }
+
+            infoQueue.add(new Info(
+                    info.count + 1,
+                    info.remainder - nums[info.leftIndex],
+                    info.leftIndex + 1,
+                    info.rightIndex
+            ));
+
+            infoQueue.add(new Info(
+                    info.count + 1,
+                    info.remainder - nums[info.rightIndex],
+                    info.leftIndex,
+                    info.rightIndex - 1
+            ));
+        }
+
+        return -1;
+    }
+
+    class Info {
+        int count;
+        int remainder;
+        int leftIndex;
+        int rightIndex;
+
+        Info(int count, int remainder, int leftIndex, int rightIndex) {
+            this.count = count;
+            this.remainder = remainder;
+            this.leftIndex = leftIndex;
+            this.rightIndex = rightIndex;
+        }
+    }
+}
